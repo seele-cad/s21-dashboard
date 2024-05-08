f5cff824468f5c182d287ae0d903993bcaea2b77
muelle4 <jan-luca.mueller@seele.com>
2024-05-08 10:53:40 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,42 +8,31 @@ from engine import *
 
 
 def check_password():
-    """Returns `True` if the user had a correct password."""
-
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
+    """Returns `True` if the user had the correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
+            del st.session_state["password"]  # Don't store the password.
         else:
             st.session_state["password_correct"] = False
 
-    # Return True if the username + password is validated.
+    # Return True if the password is validated.
     if st.session_state.get("password_correct", False):
         return True
 
-    # Show inputs for username + password.
-    login_form()
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
     if "password_correct" in st.session_state:
-        st.error("😕 User not known or password incorrect")
+        st.error("😕 Password incorrect")
     return False
 
 
 if not check_password():
-    st.stop()
+    st.stop()  # Do not continue if check_password is not True.
 
 # Configurations of Streamlit page
 st.set_page_config(
